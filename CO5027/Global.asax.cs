0a425897318fdeb31957f215cc211c181d3b0ba2
j28854-SPY <1525588@chester.ac.uk>
2017-04-04 21:20:21 +0800

    initial commit

--- CO5027/Global.asax.cs
+++ CO5027/Global.asax.cs
@@ -11,6 +11,18 @@ namespace CO5027
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            /*  Begin implementation of J querry for both maps and contact page   */
+            string JQueryVer = "3.1.1";
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-" + JQueryVer + ".min.js",
+                DebugPath = "~/Scripts/jquery-" + JQueryVer + ".js",
+                CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".min.js",
+                CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-" + JQueryVer + ".js",
+                CdnSupportsSecureConnection = true,
+                LoadSuccessExpression = "window.jQuery"
+            });
+            /*  End implementation of J querry for both maps and contact page   */
         }
     }
 }
