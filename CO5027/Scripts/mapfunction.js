ebfb63cb0b3bcd50b4ccf94ee82f4538c9256247
j28854-SPY <1525588@chester.ac.uk>
2017-04-04 21:48:03 +0800

    initial commit

---
+++ CO5027/Scripts/mapfunction.js
@@ -0,0 +1,23 @@
+﻿function initMap() {
+    var lcbcampuslocation = { lat: 4.885, lng: 114.931 };
+    var twomilesaway = { lat: 4.888, lng: 114.939 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: lcbcampuslocation
+    });
+
+    var markercampus = new google.maps.Marker({
+        position: lcbcampuslocation,
+        map: map,
+        draggable: true,
+        title: 'Safety & Protective Equipment Shop'
+    });
+
+    var marker2milescampus = new google.maps.Marker({
+        position: twomilesaway,
+        map: map,
+        draggable: true,
+        title: '2 miles of area'
+    });
+}
