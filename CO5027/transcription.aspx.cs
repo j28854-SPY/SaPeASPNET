4015b4de288512b5098a446d455c2f388bcd69d0
j28854-SPY <1525588@chester.ac.uk>
2017-05-17 06:23:03 +0800

    Initial Commit Final

--- CO5027/transcription.aspx.cs
+++ CO5027/transcription.aspx.cs
@@ -31,20 +31,20 @@ namespace CO5027
         private void list_paid_item(string customer_carted_id)
         {
             item_pushcart_model pcModel = new item_pushcart_model();
-            double subTotal = 0;
+            double must_paid = 0;
 
             /*  carted item displayed in table  */
             List<item_pushcart> paid_list = pcModel.carted_item(customer_carted_id);
-            generate_carted_table(paid_list, out subTotal);
+            generate_carted_table(paid_list, out must_paid);
 
             /*  calculations made for total must paid   */
-            double total_overall = subTotal + 9.75;
+            double total_overall = must_paid + 9.75;
 
-            literal_total_item.Text = "BND " + subTotal;
+            literal_total_item.Text = "BND " + must_paid;
             literal_overall.Text = "BND " + total_overall;
 
             /*  Add paypal button to checkout */
-            string paypal = paypal_generated_event(subTotal);
+            string paypal = paypal_generated_event(must_paid);
             literal_paypal_checkout.Text = paypal;
         }
 
@@ -56,21 +56,21 @@ namespace CO5027
 
 
 
-        private string paypal_generated_event(double subTotal)
+        private string paypal_generated_event(double must_paid)
         {
-            //Set Paypal parameters
+            /*  list object for paypal  */
             string paypal = string.Format(
                 @"<script async='async' src='https://www.paypalobjects.com/js/external/paypal-button.min.js?merchant=testlcbcompsci@gmail.com' 
                 data-button='buynow' 
                 data-name='Safety & Protective Equipments Purchases' 
                 data-quantity=1
                 data-amount='{0}' 
-                data-tax='{1}'
+                data-tax='{0}'
                 data-shipping='9.75'
                 data-callback='http://localhost:56577/Pages/Authentication/ppco.aspx'
                 data-sendback='http://localhost:56577/Pages/Authentication/ppco.aspx'
                 data-env='sandbox'>
-             </script>", subTotal);
+             </script>", must_paid, (must_paid));
 
             return paypal;
         }
@@ -125,9 +125,9 @@ namespace CO5027
 
 
 
-        private void generate_carted_table(IEnumerable<item_pushcart> paid_list, out double subTotal)
+        private void generate_carted_table(IEnumerable<item_pushcart> paid_list, out double must_paid)
         {
-            subTotal = new double();
+            must_paid = new double();
             item_table_model model = new item_table_model();
 
             foreach (item_pushcart carted in paid_list)
@@ -240,8 +240,8 @@ namespace CO5027
                 table.Rows.Add(row2);
                 transcription_page_panel.Controls.Add(table);
 
-               
-                subTotal += (carted.Total * (double)item.ItemCostBND);
+
+                must_paid += (carted.Total * (double)item.ItemCostBND);
             }
 
             
